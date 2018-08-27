0c7749c94044887a044fc4eaf0ec3ec53d83d69d
like3734 <eoajfl3734@naver.com>
2018-08-27 20:08:20 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.lang.Math;
+
+
+public class Main{
+    static int n;
+    static int [][] list;
+    
+// Main
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        
+        list = new int[n][2];
+        
+        for(int i=0; i<n; i++) {
+        	list[i][0] = sc.nextInt(); list[i][1] = sc.nextInt();
+        }
+        
+        Arrays.sort(list, new Comparator<int []>() {
+        	public int compare(int a[], int b[]) {
+        		if(a[0] == b[0]) return Integer.compare(a[1], b[1]);
+        		else return Integer.compare(a[0], b[0]);
+        	}
+        });
+        
+        for(int i=0; i<n; i++) {
+        	System.out.println(list[i][0] + " " + list[i][1]);
+        }
+    }
+}
